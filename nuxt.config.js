33cd3f5484efd882f44d411ff1c03847dcd248cc
mr.linjunzeng@foxmail <mr.linjunzeng@foxmail.com>
2018-01-23 00:06:36 +0800

    less,rem

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,42 +1,45 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'mall.wttai.com',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  css: [
-    {src: '~assets/less/base.less', lang: 'less'}
-  ],
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
-  build: {
     /*
-    ** Run ESLint on save
+    ** Headers of the page
     */
-    vendor:['~/assets/js/lib-flexible.js'],
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+    head: {
+        title: 'mall.wttai.com',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    css: [
+        {src: '~assets/less/base.less', lang: 'less'}
+    ],
+    /*
+    ** Customize the progress bar color
+    */
+    loading: { color: '#3B8070' },
+    /*
+    ** Build configuration
+    */
+    plugins: [
+        {src: '~static/js/lib-flexible.js', ssr: false},
+        '~assets/js/http.js'
+    ],
+    build: {
+        /*
+        ** Run ESLint on save
+        */
+        extend (config, { isDev, isClient }) {
+            if (isDev && isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+        }
     }
-  }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,43 @@
 <template>
-  <section class="container">
-    <div class="num">123
-    </div>
-  </section>
+    <section class="container">
+        <div class="test">Hello {{title}}</div>
+        <div v-for="(index,key) in query" :key="key">{{key}}---{{index}}</div>
+        <div v-for="(index,key) in arr" :key="index">{{key}}---{{index}}</div>
+        <div v-on:click="isclick">{{name}}</div>
+        <i class="iconfont">&#xe65d;</i>
+    </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-
+    name: 'index',
+    head: {
+        title: '首页'
+    },
+    asyncData(content) {
+        //console.log(http);
+        return {
+            title: 'World',
+            arr: [12,22,32,42,52],
+            query: content.query
+        }
+    },
+    data () {
+        return {
+            name: 'default'
+        }
+    },
+    methods: {
+        isclick: function(){
+            this.name = this.name == 'change' ? 'default' : 'change';
+        }
+    }
 }
 </script>
+<style lang='less'>
+ @import "~assets/less/common.less";
+.test{
+    font-size: 32*@rem;
+    color: @color;
+}
+</style>
